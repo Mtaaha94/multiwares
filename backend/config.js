6982f8bde0280cc393c1b4e532389fb439926079
Mtaaha94 <muhammadtaaha.94@gmail.com>
2020-12-06 20:29:27 +0500

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    secret: 'MY SECRET',
+    DB_URI_DEV:'mongodb+srv//taaha123:<password>@cart.dujex.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    DB_URI:'mongodb+srv://taaha123:<password>@cart.dujex.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    
+    
+}
